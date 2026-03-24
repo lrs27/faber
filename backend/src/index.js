d69ccaa49ac17b69e50c28aa847f592988acc2a0
daydre4mX <zerubbabelashenafi@gmail.com>
2026-03-24 01:23:46 -0400

    Add 2 routes to access users

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { Prisma, PrismaClient } from '@prisma/client';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const prisma = new PrismaClient;
 
 // Middleware
 app.use(cors());
@@ -20,6 +23,26 @@ app.get("/api/health", (req, res) => {
 app.get("/api", (req, res) => {
   res.json({ message: "Welcome to Portfolio Builder API" });
 });
+
+// Return user data
+app.get("/api/users/:userId", async (req, res) => {
+  const userId = req.params['userId'];
+  const user = await prisma.users.findUnique({
+    where: {userId: userId}
+  });
+
+  res.json(user)
+});
+
+app.get("/api/users/:userId/portfolios", async (req, res) => {
+  const userId = req.params['userId'];
+  const userPortolios = await prisma.portfolios.findMany({
+    where: {userId: userId}
+  });
+
+  res.json(userPortolios);
+
+})
 
 // Error handling middleware
 app.use((err, req, res, next) => {
